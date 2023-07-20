7450326b48bbf049c4f5840ca1b39ffdca74a537
zhenbaizhongxue <140051181+zhenbaizhongxue@users.noreply.github.com>
2023-07-21 00:23:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// 获取音乐播放器元素
-const bgMusic = document.getElementById('bg-music');
 
-// 获取音乐播放/暂停按钮元素
-const playPauseBtn = document.getElementById('play-pause-btn');
-
-// 控制音乐播放/暂停
-playPauseBtn.addEventListener('click', function() {
-    if (bgMusic.paused) {
-        bgMusic.play();
-        playPauseBtn.textContent = '暂停';
-    } else {
-        bgMusic.pause();
-        playPauseBtn.textContent = '播放';
-    }
-});
